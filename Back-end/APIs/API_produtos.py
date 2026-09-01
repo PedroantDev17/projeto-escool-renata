c7cb6e1be7487a4d28419f7b83178966a91bc1af
Paulo Dev TI <pr.riber2023@gmail.com>
2026-09-01 16:54:34 -0300

    API - Procura Produtos
    API que busca as informações do produto fornecido pelo cliente!

--- Back-end/APIs/API_produtos.py
+++ Back-end/APIs/API_produtos.py
@@ -1,7 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
-
+import serpapi
 
 app = Flask(__name__)
 CORS(app)
@@ -18,14 +18,33 @@ def lista_produtos():
     print(info)
     produto = info['nomeProduto']
 
-    url_shopee = "https://partner.shopeemobile.com/api/v2/product/get_category?access_token=access_token&language=zh-hans&partner_id=partner_id&shop_id=shop_id&sign=sign&timestamp=timestamp"
-    payload={}
-    headers = {
 
-    }
-    response = requests.request("GET",url_shopee,headers=headers, data=payload, allow_redirects=False)
-    print(response)
-    return 'Produtos - Return'
+    client = serpapi.Client(api_key="a8c241eafd2cd20e0de947b1f9bdeef39585c2615f2e707a173cc7319ae2d5b2")
+    results = client.search({
+        "engine": "google_shopping",
+        "q": produto,
+        "gl": "br",
+        "hl": "pt-br"
+    })
+
+
+    produtos_encontrados = results["shopping_results"]
+
+    for item in produtos_encontrados:
+        posicao = item.get("position")
+        Icon_loja = item.get("source_icon")
+        title = item.get("title")
+        preco = item.get("price")
+        loja = item.get("source")
+        link = item.get("product_link")
+
+    return jsonify({"resposta": "200",
+                    "posicao": posicao,
+                    "icon_loja": Icon_loja,
+                    "titulo": title,
+                    "preco": preco,
+                    "loja": loja,
+                    "link": link})
 
 if __name__ == '__main__':
     app.run(debug=True )
