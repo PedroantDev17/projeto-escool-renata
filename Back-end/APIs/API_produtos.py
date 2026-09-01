5ac3e669d6abd3f094d3b410dad79b592e91f203
Paulo Dev TI <pr.riber2023@gmail.com>
2026-08-31 22:30:27 -0300

    API - Produtos

--- Back-end/APIs/API_produtos.py
+++ Back-end/APIs/API_produtos.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+import requests
 
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/produtos', methods=['GET'])
+@app.route('/produtos', methods=['POST'])
 def lista_produtos():
+    # Pegando informações na API
+    url = 'https://commute-doormat-operator.ngrok-free.dev/produtos' 
+    
+    resposta = requests.get(url)
+    print(resposta.status_code)
+
+    info = request.get_json()
+    print(info)
+    produto = info['nomeProduto']
+
+    url_shopee = "https://partner.shopeemobile.com/api/v2/product/get_category?access_token=access_token&language=zh-hans&partner_id=partner_id&shop_id=shop_id&sign=sign&timestamp=timestamp"
+    payload={}
+    headers = {
+
+    }
+    response = requests.request("GET",url_shopee,headers=headers, data=payload, allow_redirects=False)
+    print(response)
     return 'Produtos - Return'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
+    app.run(debug=True )
